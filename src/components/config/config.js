355ea1888c97219bbd88059b60c239eacde65486
SashaLeshiy <zomlesh@yandex.ru>
2023-09-27 12:47:04 +0300

    config default

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,4 +1,4 @@
-export const FLOORS = [1, 2, 3, 4, 5, 6, 7, 8]
+export const FLOORS = [1, 2, 3, 4, 5]
 
 export const ELEVATORS = [
     {
@@ -9,20 +9,20 @@ export const ELEVATORS = [
         wait: false,
         park: 1
     },
-    {
-        id: 2, 
-        floor: 1,
-        direction: '',
-        target: 0,
-        wait: false,
-        park: 1
-    },
-    {
-        id: 3, 
-        floor: 1,
-        direction: '',
-        target: 0,
-        wait: false,
-        park: 1
-    },
+    // {
+    //     id: 2, 
+    //     floor: 1,
+    //     direction: '',
+    //     target: 0,
+    //     wait: false,
+    //     park: 1
+    // },
+    // {
+    //     id: 3, 
+    //     floor: 1,
+    //     direction: '',
+    //     target: 0,
+    //     wait: false,
+    //     park: 1
+    // },
 ]
